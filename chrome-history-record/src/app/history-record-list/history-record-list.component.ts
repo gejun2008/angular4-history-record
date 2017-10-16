fea2f7ffde2c5a13ecf1c5ca13843d9db7e75021
junge <junge@viatech.com>
2017-10-16 18:42:40 +0800

    [phase1]format file --done

--- chrome-history-record/src/app/history-record-list/history-record-list.component.ts
+++ chrome-history-record/src/app/history-record-list/history-record-list.component.ts
@@ -6,107 +6,107 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-record-list.component.css']
 })
 export class HistoryRecordListComponent implements OnInit {
-  listItems:any;
+  listItems: any;
 
   constructor() { }
 
   ngOnInit() {
     const mockData = [
       {
-        date:'2017/9/25',
-        websites:[{
-          time:"11:28",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+        date: '2017/9/25',
+        websites: [{
+          time: "11:28",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         },
         {
-          time:"13:12",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+          time: "13:12",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         },
         {
-          time:"13:08",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+          time: "13:08",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         },
         {
-          time:"13:24",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+          time: "13:24",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         }
-      ]
+        ]
       },
       {
-        date:'2017/9/24',
-        websites:[{
-          time:"8:09",
-          icon:"assets/images/favicon.ico",
-          name:"angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
-          site:"https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
-          domain:"github.com"
+        date: '2017/9/24',
+        websites: [{
+          time: "8:09",
+          icon: "assets/images/favicon.ico",
+          name: "angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
+          site: "https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
+          domain: "github.com"
         },
         {
-          time:"19:34",
-          icon:"assets/images/favicon.ico",
-          name:"angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
-          site:"https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
-          domain:"github.com"
+          time: "19:34",
+          icon: "assets/images/favicon.ico",
+          name: "angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
+          site: "https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
+          domain: "github.com"
         },
         {
-          time:"16:08",
-          icon:"assets/images/favicon.ico",
-          name:"angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
-          site:"https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
-          domain:"github.com"
+          time: "16:08",
+          icon: "assets/images/favicon.ico",
+          name: "angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
+          site: "https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
+          domain: "github.com"
         },
         {
-          time:"13:24",
-          icon:"assets/images/favicon.ico",
-          name:"angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
-          site:"https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
-          domain:"github.com"
+          time: "13:24",
+          icon: "assets/images/favicon.ico",
+          name: "angular4-history-record/chrome-history-record at master · gejun2008/angular4-history-record",
+          site: "https://github.com/gejun2008/angular4-history-record/tree/master/chrome-history-record",
+          domain: "github.com"
         }
-      ]
+        ]
       },
       {
-        date:'2017/9/29',
-        websites:[{
-          time:"18:09",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+        date: '2017/9/29',
+        websites: [{
+          time: "18:09",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         },
         {
-          time:"19:34",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+          time: "19:34",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         },
         {
-          time:"19:08",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+          time: "19:08",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         },
         {
-          time:"23:24",
-          icon:"assets/images/favicon.ico",
-          name:"favicon.ico (32×32)",
-          site:"https://assets-cdn.github.com/favicon.ico",
-          domain:"assets-cdn.github.com"
+          time: "23:24",
+          icon: "assets/images/favicon.ico",
+          name: "favicon.ico (32×32)",
+          site: "https://assets-cdn.github.com/favicon.ico",
+          domain: "assets-cdn.github.com"
         }
-      ]
+        ]
       }
     ]
 
